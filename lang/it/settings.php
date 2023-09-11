fd783f93b4d23572f75035465f1d11f31e83f951
Andrea <andreabz@users.noreply.github.com>
2023-09-11 22:06:24 +0200

    italian translation of lang folder

---
+++ lang/it/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['restrict_admin'] = 'Le regole di pubblicazione vengono applicate anche agli amministratori. Togli la spunta se gli amministratori devono visualizzare le revisioni non pubblicate, anche se non hanno i permessi di pubblicazione.';
+$lang['email_enable'] = 'Invia e-mail';
+$lang['email_status'] = 'Modifiche dello stato che attivano le notifiche via e-mail';
+$lang['compact_view'] = 'Banner compatto';
